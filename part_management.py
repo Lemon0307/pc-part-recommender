22d4e0b3940952ec82cf9a5a80a9a32bb7f5da6c
U-PORKGUY69\cheun <cheungcheukheigordon@gmail.com>
2025-12-28 22:56:42 +0000

    fixed code

--- part_management.py
+++ part_management.py
@@ -31,52 +31,52 @@ def add_part():
 
     return jsonify({"message": f"{part_type} added successfully"}), 201
 
-@part_management.route('/create_empty_order', methods=['POST'])
-def create_empty_order():
+@part_management.route('/create_empty_build', methods=['POST'])
+def create_empty_build():
     driver = get_driver()
-    order_id = str(uuid.uuid4())
+    build_id = str(uuid.uuid4())
 
-    query = "CREATE (o:Order {status: 'pending', created_at: datetime(), order_id: $order_id}) RETURN $order_id AS order_id"
-    result = driver.execute_query(query, order_id=order_id)
+    query = "CREATE (o:Build {status: 'pending', created_at: datetime(), build_id: $build_id}) RETURN $build_id AS build_id"
+    result = driver.execute_query(query, build_id=build_id)
 
-    return jsonify({"order_id": result.records[0]["order_id"]}), 201
+    return jsonify({"build_id": result.records[0]["build_id"]}), 201
 
-@part_management.route('/add_part_to_order', methods=['POST'])
-def add_part_to_order():
+@part_management.route('/add_part_to_build', methods=['POST'])
+def add_part_to_build():
     driver = get_driver()
     
     #get data from request
     data = request.json
     part_id = data.get("part_id")
-    order_id = data.get("order_id")
+    build_id = data.get("build_id")
     quantity = data.get("quantity")
     
-    # checks if parts and order exist
+    # checks if parts and build exist
     check_part_exists = "MATCH (p {part_id: $part_id}) RETURN p"
     part_result = driver.execute_query(check_part_exists, part_id=part_id)
-    check_order_exists = "MATCH (o:Order {order_id: $order_id}) RETURN o"
-    order_result = driver.execute_query(check_order_exists, order_id=order_id)  
+    check_build_exists = "MATCH (o:Build {build_id: $build_id}) RETURN o"
+    build_result = driver.execute_query(check_build_exists, build_id=build_id)  
 
-    if not order_result.records[0] and not part_result.records[0]:
-        return jsonify({"error": "Part or order does not exist"}), 404
+    if not build_result.records and not part_result.records:
+        return jsonify({"error": "Part or build does not exist"}), 404
     
-    # checks if part is already in order
-    check_if_already_in_order = driver.execute_query("""
-    MATCH (o:Order {order_id: $order_id})-[r:CONTAINS]->(p {part_id: $part_id})
+    # checks if part is already in build
+    check_if_already_in_build = driver.execute_query("""
+    MATCH (o:Build {build_id: $build_id})-[r:CONTAINS]->(p {part_id: $part_id})
     RETURN r 
-    """, order_id=order_id, part_id=part_id)
+    """, build_id=build_id, part_id=part_id)
 
-    if check_if_already_in_order.records[0]:
-        return jsonify({"error": "Part already in order"}), 400
+    if check_if_already_in_build.records:
+        return jsonify({"error": "Part already in build"}), 400
 
     #builds and executes query
     query = """
-    MATCH (p {part_id: $part_id}), (o:Order {order_id: $order_id})
+    MATCH (p {part_id: $part_id}), (o:Build {build_id: $build_id})
     CREATE (o)-[:CONTAINS {quantity: $quantity}]->(p)
     """
 
-    driver.execute_query(query, part_id=part_id, order_id=order_id, quantity=quantity)
-    return jsonify({"message": "Part added to order successfully"}), 200
+    driver.execute_query(query, part_id=part_id, build_id=build_id, quantity=quantity)
+    return jsonify({"message": "Part added to build successfully"}), 200
 
 # @part_management.route('/testing', methods=['POST'])
 # def testing():
